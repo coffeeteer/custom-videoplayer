fc819e1c92872db7a78bc42c3d1849c014784362
Coffeeteer <tgrieshiemer@gmail.com>
2017-01-02 18:50:19 -0500

    project complete - next challenge is to add a full screen window option

--- js/script.js
+++ js/script.js
@@ -33,6 +33,11 @@ function handleProgress(){
 	progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+	const scrubTime  = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -43,4 +48,10 @@ toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = true;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () =>  true);
+progress.addEventListener('mouseup', () => false);
